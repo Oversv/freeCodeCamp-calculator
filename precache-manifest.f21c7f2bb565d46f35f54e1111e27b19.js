bd0dcfbc6303af343e24ffcd38f6fd85cc7a8570
Oversv <albertomdv@gmail.com>
2020-03-27 12:42:42 +0000

    Updates

---
+++ precache-manifest.f21c7f2bb565d46f35f54e1111e27b19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0a064043ec799d66bd55630db75ddc",
+    "url": "/freeCodeCamp-calculator/index.html"
+  },
+  {
+    "revision": "a692b6acd9612f50d991",
+    "url": "/freeCodeCamp-calculator/static/css/main.078d5c5e.chunk.css"
+  },
+  {
+    "revision": "feafae2a9d6c70488a38",
+    "url": "/freeCodeCamp-calculator/static/js/2.47d743fa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/freeCodeCamp-calculator/static/js/2.47d743fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a692b6acd9612f50d991",
+    "url": "/freeCodeCamp-calculator/static/js/main.6a04b6cf.chunk.js"
+  },
+  {
+    "revision": "617f620cc449193314f0",
+    "url": "/freeCodeCamp-calculator/static/js/runtime-main.bd7632ca.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/freeCodeCamp-calculator/static/media/digital-7.1e670d88.ttf"
+  }
+]);
